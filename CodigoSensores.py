f8a8ca55c8ff577fbe5937c7d054c11b3395a685
J0t4py <89493486+J0t4py@users.noreply.github.com>
2022-06-22 20:34:01 -0300

    Create CodigoSensores.py

--- CodigoSensores.py
+++ CodigoSensores.py
@@ -6,8 +6,8 @@ from sensirion_i2c_driver import LinuxI2cTransceiver, I2cConnection, CrcCalculat
 from sensirion_i2c_adapter.i2c_channel import I2cChannel
 from sensirion_i2c_scd30.device import Scd30Device
 class SCD30:
-    def medicaoSCD30(self):
-        with LinuxI2cTransceiver('/dev/i2c-1') as i2c_transceiver:
+    def medicaoSCD30(self): #Faz a medição do SCD30
+        with LinuxI2cTransceiver('/dev/i2c-1') as i2c_transceiver: #Talvez seja necessário mudar o bus do i2c, para ver qual é o correto utilize o i2c tools como exemplificado no Readme
             channel = I2cChannel(I2cConnection(i2c_transceiver),
                                 slave_address=0x61,
                                 crc=CrcCalculator(8, 0x31, 0xff, 0x0))
@@ -20,7 +20,7 @@ class SCD30:
                 ...
 
             (major, minor) = sensor.read_firmware_version()
-            print(f"firmware version major: {major}; minor: {minor};")
+            print(f"firmware version major: {major}; minor: {minor};") #Mostra a versão do firmware
             sensor.start_periodic_measurement(0)
             for i in range(30):
                 try:
@@ -32,7 +32,7 @@ class SCD30:
             sensor.soft_reset()
         
 class US100:
-    def medicaoUS100(self):
+    def medicaoUS100(self): #Essa função faz a medição do US100
         # Caso ocorra algum erro, troque a porta que o USB está conectado ou troque a porta especificada na variável UART
         uart = serial.Serial("/dev/ttySAC0", baudrate=9600, timeout=1)
         # uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
